3bb0054fca6fb7155aaad4a65f2f10d42367a70b
Correy Everett <correy.everett@gmail.com>
2021-02-09 15:42:15 -0600

    post controllers

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -2,6 +2,8 @@ package com.codeup.springblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,6 +13,24 @@ public class PostController {
     @ResponseBody
     public String indexPage() {
         return "This is and index page!";
+    }
+
+    @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String post() {
+        return "You are viewing an singular post!";
+    }
+
+    @GetMapping("/posts/create")
+    @ResponseBody
+    public String postForm() {
+        return "View post form!";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String createPost() {
+        return "Create post!";
     }
 
 
