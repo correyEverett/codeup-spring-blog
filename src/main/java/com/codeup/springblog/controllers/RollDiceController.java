4cccfb7202537c224cf7e1bcdde94d5735e63df1
Correy Everett <correy.everett@gmail.com>
2021-02-11 09:23:31 -0600

    update RollDiceController

--- src/main/java/com/codeup/springblog/controllers/RollDiceController.java
+++ src/main/java/com/codeup/springblog/controllers/RollDiceController.java
@@ -3,22 +3,32 @@ package com.codeup.springblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RollDiceController {
 
     @GetMapping("/roll-dice")
-    public String rollDice() {
+    public String showRollDice() {
         return "roll-dice";
     }
 
-    @GetMapping("/roll-dice/{n}")
-    public String guess(@RequestParam(name = "n") int number, Model model) {
+    @GetMapping("/roll-dice/{guess}")
+    public String rollDice(@PathVariable int guess, Model model) {
         int randomNumber = (int) Math.floor(Math.random() * 5) + 1;
+        String message;
 
-        model.addAttribute("n", "You guessed " + number);
-        model.addAttribute(randomNumber);
+        if(guess == randomNumber){
+            message = "Yay, you guessed correctly!";
+        } else {
+            message = "Sorry, try again!";
+        }
+
+        model.addAttribute("guess", guess);
+        model.addAttribute("randomNumber",randomNumber);
+        model.addAttribute("message", message);
+
         return "roll-dice";
     }
 }
