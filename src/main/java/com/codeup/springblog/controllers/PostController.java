26a8160f10cad0162f231ad1bcce6d29b7bd7fda
Correy Everett <correy.everett@gmail.com>
2021-02-26 08:47:19 -0600

    assign user to post

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
+import com.codeup.springblog.models.User;
 import com.codeup.springblog.repositries.PostRepository;
 import com.codeup.springblog.repositries.UserRepository;
 import org.springframework.stereotype.Controller;
@@ -13,53 +14,43 @@ import java.util.ArrayList;
 public class PostController {
 
     private final PostRepository postsDao;
-    private final UserRepository userDao;
+    private final UserRepository usersDao;
 
-    public PostController(PostRepository postsDao, UserRepository userDao) {
+    public PostController(PostRepository postsDao, UserRepository usersDao) {
         this.postsDao = postsDao;
-        this.userDao = userDao;
+        this.usersDao = usersDao;
     }
 
-    @GetMapping("/post")
-    @ResponseBody
-    public String indexPage() {
-        return "This is and index page!";
-    }
-
-    @GetMapping("/posts/create")
-    @ResponseBody
-    public String postForm() {
-        return "View post form!";
-    }
-
-    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
-    @ResponseBody
-    public String createPost() {
-        return "Create post!";
-    }
-
-    @GetMapping("index")
-    public String showAllPosts(Model model) {
-
-        ArrayList<Post> allPosts = new ArrayList<>();
-        Post post1 = new Post(1, "title1", "First Post");
-        Post post2 = new Post(2, "title2", "Second Post");
-
-        allPosts.add(post1);
-        allPosts.add(post2);
-
-        model.addAttribute("posts", allPosts);
-
+    @GetMapping("/posts")
+    public String viewAllPosts(Model model) {
+        model.addAttribute("posts", postsDao.findAll());
         return "posts/index";
     }
 
     @GetMapping("/posts/{id}")
-    public String showIndividualPost(Model model, @PathVariable String id) {
-        Post singlePost1 = new Post(1,"title" , "body");
-
-        model.addAttribute("post", singlePost1);
-
+    public String viewSingularPost(Model model, @PathVariable Long id) {
+        Post post = postsDao.getOne(id);
+        model.addAttribute("post", post);
         return "posts/show";
+    }
+
+    @GetMapping("/posts/create")
+    public String postForm(Model model){
+        model.addAttribute("post", new Post());
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@RequestParam String title, @RequestParam String body) {
+        Post post = new Post();
+        post.setTitle(title);
+        post.setBody(body);
+
+        User user = usersDao.findAll().get(0);
+        post.setUser(user);
+
+        postsDao.save(post);
+        return "redirect:/posts/" + post.getId();
     }
 
     @PostMapping("/posts/{id}/edit")
