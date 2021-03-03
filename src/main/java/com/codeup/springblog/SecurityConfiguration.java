49b04f548f4629d6f365e8342eb6dd4f96ef0184
Correy Everett <correy.everett@gmail.com>
2021-03-03 09:17:50 -0600

    updated SecurityConfiguration class

--- src/main/java/com/codeup/springblog/SecurityConfiguration.java
+++ src/main/java/com/codeup/springblog/SecurityConfiguration.java
@@ -26,7 +26,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/")
+                .defaultSuccessUrl("/posts")
                 .permitAll()
                 .and()
                 .logout()
