fddad65df5d2f0b11e01a51b222c4f419bc91421
Correy Everett <correy.everett@gmail.com>
2021-03-03 09:25:53 -0600

    update HomeController

--- src/main/java/com/codeup/springblog/controllers/HomeController.java
+++ src/main/java/com/codeup/springblog/controllers/HomeController.java
@@ -9,33 +9,42 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 @Controller
 public class HomeController {
 
     private final UserRepository usersDao;
     private final PostRepository postsDao;
+    private final PasswordEncoder encoder;
 
-    public HomeController(UserRepository usersDao, PostRepository postsDao) {
+    public HomeController(UserRepository usersDao, PostRepository postsDao, PasswordEncoder encoder) {
         this.usersDao = usersDao;
         this.postsDao = postsDao;
+        this.encoder = encoder;
     }
 
 
     @GetMapping("/")
+    public String home(){
+        return "redirect:login";
+    }
+    @GetMapping("/login")
     public String showLoginForm() {
         return "login";
     }
-
     @GetMapping("/sign-up")
     public String showSignUpForm(Model model) {
         model.addAttribute("user", new User());
         return "sign-up";
     }
-
-//    @PostMapping("/sign-up")
-//    public String createUser(@ModelAttribute User user) {
-//
-//    }
+    @PostMapping("/sign-up")
+    public String signUpUser(@ModelAttribute User user) {
+        String hash = encoder.encode(user.getPassword());
+        user.setPassword(hash);
+        usersDao.save(user);
+        return "redirect:/login";
+    }
 
 
 
